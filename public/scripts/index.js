6e48129d7fffff3c0d507d9f390c972f1e26b93a
FReyeSFT <fernandoreyezpinto@gmail.com>
2022-05-09 08:17:13 -0500

    el menu desplegable queda terminado

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,6 +3,10 @@ const buttonNav02 = document.querySelector('.showMenu02');
 const menuFloatInmobiliaria = document.querySelector('.nav__menu--float--inmobiliaria');
 const menuFloatAbogado = document.querySelector('.nav__menu--float--abogado');
 
+const navMenu = document.querySelector('#nav__menu--mobile');
+const navMenuShow = document.querySelector('#nav__menu--mobile--open');
+
+
 buttonNav01.addEventListener('click', () => {
     menuFloatInmobiliaria.classList.toggle('activo');
     if (menuFloatInmobiliaria.classList.contains('activo')) {
@@ -39,3 +43,16 @@ function changeWallpaper() {
     }, 7000);
   }
   changeWallpaper();
+
+navMenu.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+    if (navMenu.classList.contains("active")) {
+        navMenuShow.style.display = 'block';
+    }
+    else {
+        navMenuShow.style.display = 'none';
+    }
+})
+
+
+
