3c54bce45cb767626b551f7b7e90cd3c1875aab0
FReyeSFT <fernandoreyezpinto@gmail.com>
2022-05-10 13:36:43 -0500

    termino el formulario de contacto

--- public/scripts/abogado.js
+++ public/scripts/abogado.js
@@ -6,6 +6,17 @@ const menuFloatAbogado = document.querySelector('.nav__menu--float--abogado');
 const navMenu = document.querySelector('#nav__menu--mobile');
 const navMenuShow = document.querySelector('#nav__menu--mobile--open');
 
+function sendMessage() {
+    let name = document.querySelector(".name").value;
+    let lastName = document.querySelector(".last-name").value;
+    let message = document.querySelector(".input-message").value;
+    let strigToSend = `${name} ${lastName} dice: ${message}`;
+    document.querySelector(".float01").href = `https://api.whatsapp.com/send?phone=+1 3124147546&text=${strigToSend}`;
+}
+
+const inputSend = document.querySelector(".form__send");
+
+inputSend.addEventListener('click', sendMessage);
 
 buttonNav01.addEventListener('click', () => {
     menuFloatInmobiliaria.classList.toggle('activo');
