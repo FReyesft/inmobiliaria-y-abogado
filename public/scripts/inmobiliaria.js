7510078116c865f2f629f78cd181b91f2e945ff7
FReyeSFT <fernandoreyezpinto@gmail.com>
2022-05-12 17:44:41 -0500

    formulario inmobiliaria

--- public/scripts/inmobiliaria.js
+++ public/scripts/inmobiliaria.js
@@ -6,6 +6,26 @@ const menuFloatAbogado = document.querySelector('.nav__menu--float--abogado');
 const navMenu = document.querySelector('#nav__menu--mobile');
 const navMenuShow = document.querySelector('#nav__menu--mobile--open');
 
+const body = document.querySelector('body');
+const buttonsContact01 = document.querySelector('.button01');
+const buttonsContact02 = document.querySelector('.button02');
+const buttonsContact03 = document.querySelector('.button03');
+const form = document.querySelector('.section__container--form');
+const closeForm = document.querySelector('.fa-xmark');
+function showOverlay() {
+    form.classList.toggle('show');
+    if (form.classList.contains('show')) {
+        form.style.display = "block";
+    }
+    else {
+        form.style.display = "none";
+    }
+}
+closeForm.addEventListener('click', showOverlay);
+buttonsContact01.addEventListener('click', showOverlay);
+buttonsContact02.addEventListener('click', showOverlay);
+buttonsContact03.addEventListener('click', showOverlay);
+
 function sendMessage() {
     let name = document.querySelector(".name").value;
     let lastName = document.querySelector(".last-name").value;
